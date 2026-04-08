471de24b2ea3d36886d3a31cd364099693603788
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2026-04-09 01:57:53 +0200

    chore: add gsd

---
+++ src/lib/components/MarkdownContent.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { marked } from 'marked';
+
+	let { body }: { body: string } = $props();
+
+	let html = $derived(marked.parse(body) as string);
+</script>
+
+<div class="prose prose-invert max-w-none">
+	<!-- eslint-disable-next-line svelte/no-at-html-tags -->
+	{@html html}
+</div>
